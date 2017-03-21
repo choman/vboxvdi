be9d1b37e4d4dc838f1832872a272fb554ab9b56
Chad Homan <choman@gmail.com>
2017-03-21 10:41:17 -0500

    type in setuptools

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ sudo apt-fast dist-upgrade -y
 
 
 # install hypervisor
-sudo apt-fast install -y python-pytimeparse python-yaml python-setuptols \
+sudo apt-fast install -y python-pytimeparse python-yaml python-setuptools \
                          python-lockfile ipvsadm meld terminix
 
 sudo mkdir -p /opt/vboxvdi
