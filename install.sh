1e2124b8c801e015885b91ffae5841e955c34819
Chad Homan <choman@gmail.com>
2021-07-07 04:03:01 -0500

    Resolves linting errors

--- install.sh
+++ install.sh
@@ -25,22 +25,18 @@ sudo apt-fast install -y di git axel ssh apt-fast vlock tmux
 #####################################
 ./clean.sh
 
-
 #
 # update system
 sudo apt-fast dist-upgrade -y
 
-
 # install hypervisor
 sudo apt-fast install -y python-pytimeparse python-yaml python-setuptools pandoc \
-                         python-lockfile python-daemon ipvsadm meld terminix
-
+	python-lockfile python-daemon ipvsadm meld terminix
 
 # firewall stuff
 sudo ufw enable
 sudo ufw allow 22
 sudo ufw allow 3389
-
 
 #VBOX
 echo "deb http://download.virtualbox.org/virtualbox/debian yakkety contrib" | sudo tee /etc/apt/sources.list.d/vbox.list
@@ -48,34 +44,35 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 sudo apt-fast update -qq
 sudo apt-fast install -y virtualbox-5.1
 
-
 AFLAGS="--allow-overwrite=true --auto-file-renaming=false --conditional-get=true"
 
 aria2c -x 8 -d /tmp http://download.virtualbox.org/virtualbox/LATEST.TXT
 version=$(cat /tmp/LATEST.TXT)
-aria2c -x 8 $AFLAGS http://download.virtualbox.org/virtualbox/${version}/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack
+aria2c -x 8 "$AFLAGS" "http://download.virtualbox.org/virtualbox/${version}/Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack"
 
 # prompts for passwd
 sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.1.18.vbox-extpack
 
 #LYNIS
-aria2c -x 8 $AFLAGS https://cisofy.com/files/lynis-2.4.6.tar.gz
+aria2c -x 8 "$AFLAGS" https://cisofy.com/files/lynis-2.4.6.tar.gz
 sudo tar xvpf lynis-2.4.6.tar.gz -C /opt
 
 #
 # install pyvbox
 git clone https://github.com/mjdorma/pyvbox
-(cd pyvbox; sudo python setup.py install)
+(
+	pushd pyvbox || echo "Unable to change dirs"
+	sudo python setup.py install
+	popd || echo "Unable to return dirs"
+)
 
-
-aria2c -x 8 $AFLAGS https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip
+aria2c -x 8 "$AFLAGS" https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip
 sudo unzip -d /usr/local/bin packer_0.12.3_linux_amd64.zip
-
 
 #
 # secure system
 echo "Securing Logon Screen"
-sudo tee -a /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf > /dev/null <<EOF
+sudo tee -a /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf >/dev/null <<EOF
 allow-guest=false
 greeter-show-manual-login=true
 greeter-hide-users=true
@@ -97,25 +94,22 @@ vdipass='$6$VULZjok1$8V7YYoddIJ23UCSQYI1Xua63ES1Qs6gJuWjt2HWAZCBmjJPqndQVgWBZfJ1
 VDI_SSH_HOME=/home/VDI/.ssh
 sudo useradd -s /bin/bash -m vdiadmin -p $vdipass
 sudo usermod -a -G vboxusers vdiadmin,root
-sudo usermod -a -G vboxusers $USER
-sudo usermod -a -G vdiadmin $USER
+sudo usermod -a -G vboxusers "$USER"
+sudo usermod -a -G vdiadmin "$USER"
 
 # setup vboxvdi
 sudo mkdir -p /opt/vboxvdi
 sudo rsync -uav config.yml.tmpl ipvsadmin.sh vboxvdi /opt/vboxvdi
 sudo chown -R vdiadmin:vdiadmin /opt/vboxvdi
-echo "vdiadmin ALL=(ALL) NOPASSWD: /opt/vboxvdi/ipvsadmin.sh" | sudo tee -a  /etc/sudoers
-
+echo "vdiadmin ALL=(ALL) NOPASSWD: /opt/vboxvdi/ipvsadmin.sh" | sudo tee -a /etc/sudoers
 
 # setup /vdi directory
 sudo mkdir -v /vdi
 sudo chown -R vdiadmin:vdiadmin /vdi
 sudo -i -u vdiadmin /usr/bin/vboxmanage setproperty machinefolder /vdi
 
-
 # setup VDI users for sudo
 ##line=$(grep VDI ${KVMDIR}/hypervisors/sudoers)
 ##sudo sed  -i "/^root/ a  $line" /etc/sudoers
-
 
 sudo reboot
